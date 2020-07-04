f8e786e505e63f79582f35d9427eb13aae6999ea
gmart <martinetti.gregoire@gmail.com>
2020-07-04 16:53:45 +0200

    add test

--- cypress/integration/xxx.spec.js
+++ cypress/integration/xxx.spec.js
@@ -1,6 +1,6 @@
 
-const port = 1234;
-const url = `http://127.0.0.1:1234/${port}`;
+const port = 4567;
+const url = `http://127.0.0.1:${port}/`;
 
 describe("", () => {
     beforeEach(() => {
@@ -10,9 +10,9 @@ describe("", () => {
     // const operation = op => cy.get(".operator-pad").contains(op).click();
     // const toogleSign = () => cy.get("#signToggle").click();
     // const digit = d0 => cy.get(".pad").contains("" + d0).click();
-    // const screenIs = textToBeOnScreen =>
-    //     cy.get(".screen").should(elem => expect(elem.text())
-    //         .to.equal("" + textToBeOnScreen));
+    const clickCountIs = textToBeEqualWith =>
+        cy.get(".clickCount").should(elem => expect(elem.text())
+            .to.equal("" + textToBeEqualWith));
     // const dot = () => cy.get("#dot").click();
     // //prev was: screenIs(xxx);
     // const backSpace = () => cy.get("#backSpace").click();
@@ -20,5 +20,11 @@ describe("", () => {
     //     const calculatorWidth = calc.width()
     //     cy.get(".screen").invoke('outerWidth').should('be.lt', calculatorWidth + 1);
     // })
+    it("", () => {
+        cy.get("img").click();
+        //clickCountIs(1)
+        cy.get(".clickCount").should("contain", 1);
+    })
+
 
 });
